85591e9d992ef52e98857bbeccf497235b4645e6
za3em <za3em445439@gmail.com>
2026-02-21 19:05:13 +0300

    add file yml and sql container

--- obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88accea51e16b6f4fa5775c547bc24e9d0d670e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
